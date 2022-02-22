c24a16b78016fe861b531d40d87b1554669ec3d1
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2022-02-22 19:05:54 +0000

    add third property to main function

--- airbnb_scraper/__main__.py
+++ airbnb_scraper/__main__.py
@@ -2,8 +2,10 @@ from property.property import Property
 
 
 def main():
-    properties = [Property(
-        "https://www.airbnb.co.uk/rooms/33090114"), Property("https://www.airbnb.co.uk/rooms/50633275")
+    properties = [
+        Property("https://www.airbnb.co.uk/rooms/33571268"),
+        Property("https://www.airbnb.co.uk/rooms/33090114"),
+        Property("https://www.airbnb.co.uk/rooms/50633275")
     ]
 
     for property in properties:
